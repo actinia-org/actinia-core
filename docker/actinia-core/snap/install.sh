e02eedd3e4935f8ef5192c45d7be5f8dd368e482
Markus Neteler <neteler@gmail.com>
2019-11-12 07:37:13 +0100

    increase java_max_mem to avoid NullPointer exception (#64)
    - increase `java_max_mem` to avoid NullPointer exception in Snappy during S-1 processing
    - clean up /src to reduce image size
    - Dockerfile: SNAP wants the current folder '.' included in LD_LIBRARY_PATH
    - explicitely use bash and not sh to run snap/install.sh

--- docker/actinia-core/snap/install.sh
+++ docker/actinia-core/snap/install.sh
@@ -1,10 +1,12 @@
-# /bin/sh
+#!/bin/bash
 
 # https://senbox.atlassian.net/wiki/spaces/SNAP/pages/30539778/Install+SNAP+on+the+command+line
 # https://senbox.atlassian.net/wiki/spaces/SNAP/pages/30539785/Update+SNAP+from+the+command+line
 
 # http://step.esa.int/main/download/snap-download/
 SNAPVER=7
+# avoid NullPointer exception during S-1 processing
+java_max_mem=10G
 
 # set JAVA_HOME (done in Docker as well)
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
@@ -27,5 +29,15 @@ sh /src/snap/esa-snap_all_unix_${SNAPVER}_0.sh -q -varfile /src/snap/response.va
 /usr/local/snap/bin/snappy-conf /usr/bin/python3
 (cd /root/.snap/snap-python/snappy && python3 setup.py install)
 
+# increase the JAVA VM size to avoid NullPointer exception in Snappy during S-1 processing
+(cd /root/.snap/snap-python/snappy && sed -i "s/^java_max_mem:.*/java_max_mem: $java_max_mem/" snappy.ini)
+
+# get minor python version
+PYMINOR=$(python3 -c 'import platform; major, minor, patch = platform.python_version_tuple(); print(minor)')
+(cd /usr/local/lib/python3.$PYMINOR/dist-packages/snappy/ && sed -i "s/^java_max_mem:.*/java_max_mem: $java_max_mem/" snappy.ini)
+
 # test
 /usr/bin/python3 -c 'from snappy import ProductIO'
+
+# cleanup installer
+rm -f /src/snap/esa-snap_all_unix_${SNAPVER}_0.sh
