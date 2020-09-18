b2620a2870870f256914f547df5d83e77b7b4495
griembauer <62383722+griembauer@users.noreply.github.com>
2020-09-18 14:49:35 +0200

    Update src/actinia_core/resources/ephemeral_processing_with_export.py
    Co-authored-by: Markus Neteler <neteler@gmail.com>

--- src/actinia_core/resources/ephemeral_processing_with_export.py
+++ src/actinia_core/resources/ephemeral_processing_with_export.py
@@ -259,7 +259,7 @@ class EphemeralProcessingWithExport(EphemeralProcessing):
             args.extend(["createopt=COMPRESS=LZW,TILED=YES", "overviews=5"])
 
         # current workaround due to color table export
-        # TODO: DELETE AND TEST ONCE GDAL 3.2 HAS BEEN RELEASED
+        # TODO: DELETE AND TEST ONCE GDAL 3.1.4 HAS BEEN RELEASED
         elif format == "COG":
             args.append("-c")
 
