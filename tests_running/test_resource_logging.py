9cd370c02a244bcf3708bf15937ef21dc3c969dc
Anika Bettge <bettge@mundialis.de>
2020-09-18 07:55:05 +0200

    logging

--- tests_running/test_resource_logging.py
+++ tests_running/test_resource_logging.py
@@ -53,7 +53,7 @@ class ResourceLoggingTestCase(ActiniaResourceTestCaseBase):
         # The test user
         self.user_id = "soeren"
         self.resource_id = uuid.uuid1()
-        self.document = pickle.dumps({"Status":"running", "URL":"/bla/bla"})
+        self.document = pickle.dumps([200, {"Status":"running", "URL":"/bla/bla"}])
 
         redis_args = (global_config.REDIS_SERVER_URL, global_config.REDIS_SERVER_PORT)
         if global_config.REDIS_SERVER_PW is not None:
@@ -66,91 +66,91 @@ class ResourceLoggingTestCase(ActiniaResourceTestCaseBase):
         self.app_context.pop()
 
     # TODO
-    # def test_logging(self):
-    #
-    #     ret = self.log.commit(user_id=self.user_id,
-    #                           resource_id=self.resource_id,
-    #                           document=self.document)
-    #
-    #     self.assertTrue(ret)
-    #
-    #     ret = self.log.commit(user_id=self.user_id,
-    #                           resource_id=self.resource_id,
-    #                           document=self.document)
-    #
-    #     self.assertTrue(ret)
-    #
-    #     doc = self.log.get(user_id=self.user_id,
-    #                        resource_id=self.resource_id)
-    #     print(doc)
-    #     self.assertEqual(self.document, doc)
-    #
-    #     ret = self.log.delete(user_id=self.user_id,
-    #                           resource_id=self.resource_id)
-    #
-    #     self.assertTrue(ret)
-    #
-    #     doc = self.log.get(user_id=self.user_id,
-    #                        resource_id=self.resource_id)
-    #
-    #     self.assertEqual(None, doc)
-    #
-    #     ret = self.log.delete(user_id=self.user_id,
-    #                           resource_id=self.resource_id)
-    #
-    #     self.assertFalse(ret)
+    def test_logging(self):
+
+        ret = self.log.commit(user_id=self.user_id,
+                              resource_id=self.resource_id,
+                              document=self.document)
+
+        self.assertTrue(ret)
+
+        ret = self.log.commit(user_id=self.user_id,
+                              resource_id=self.resource_id,
+                              document=self.document)
+
+        self.assertTrue(ret)
+
+        doc = self.log.get(user_id=self.user_id,
+                           resource_id=self.resource_id)
+        print(doc)
+        self.assertEqual(self.document, doc)
+
+        ret = self.log.delete(user_id=self.user_id,
+                              resource_id=self.resource_id)
+
+        self.assertTrue(ret)
+
+        doc = self.log.get(user_id=self.user_id,
+                           resource_id=self.resource_id)
+
+        self.assertEqual(None, doc)
+
+        ret = self.log.delete(user_id=self.user_id,
+                              resource_id=self.resource_id)
+
+        self.assertFalse(ret)
 
     # TODO
-    # def test_list(self):
-    #
-    #     user = "lisa"
-    #     resource = "a"
-    #     ret = self.log.commit(user_id=user,
-    #                           resource_id=resource,
-    #                           document=self.document)
-    #     self.assertTrue(ret)
-    #
-    #     user = "franky"
-    #     resource = "a"
-    #     ret = self.log.commit(user_id=user,
-    #                           resource_id=resource,
-    #                           document=self.document)
-    #     self.assertTrue(ret)
-    #
-    #     resource = "b"
-    #     ret = self.log.commit(user_id=user,
-    #                           resource_id=resource,
-    #                           document=self.document)
-    #     self.assertTrue(ret)
-    #
-    #     user = "klaus"
-    #     resource = "a"
-    #     ret = self.log.commit(user_id=user,
-    #                           resource_id=resource,
-    #                           document=self.document)
-    #     self.assertTrue(ret)
-    #
-    #     resource = "b"
-    #     ret = self.log.commit(user_id=user,
-    #                           resource_id=resource,
-    #                           document=self.document)
-    #     self.assertTrue(ret)
-    #
-    #     resource = "c"
-    #     ret = self.log.commit(user_id=user,
-    #                           resource_id=resource,
-    #                           document=self.document)
-    #     self.assertTrue(ret)
-    #
-    #     ret = self.log.get_user_resources("lisa")
-    #     self.assertEqual(len(ret), 1)
-    #     print(ret)
-    #
-    #     ret = self.log.get_user_resources("franky")
-    #     print(ret)
-    #
-    #     ret = self.log.get_user_resources("klaus")
-    #     print(ret)
+    def test_list(self):
+
+        user = "lisa"
+        resource = "a"
+        ret = self.log.commit(user_id=user,
+                              resource_id=resource,
+                              document=self.document)
+        self.assertTrue(ret)
+
+        user = "franky"
+        resource = "a"
+        ret = self.log.commit(user_id=user,
+                              resource_id=resource,
+                              document=self.document)
+        self.assertTrue(ret)
+
+        resource = "b"
+        ret = self.log.commit(user_id=user,
+                              resource_id=resource,
+                              document=self.document)
+        self.assertTrue(ret)
+
+        user = "klaus"
+        resource = "a"
+        ret = self.log.commit(user_id=user,
+                              resource_id=resource,
+                              document=self.document)
+        self.assertTrue(ret)
+
+        resource = "b"
+        ret = self.log.commit(user_id=user,
+                              resource_id=resource,
+                              document=self.document)
+        self.assertTrue(ret)
+
+        resource = "c"
+        ret = self.log.commit(user_id=user,
+                              resource_id=resource,
+                              document=self.document)
+        self.assertTrue(ret)
+
+        ret = self.log.get_user_resources("lisa")
+        self.assertEqual(len(ret), 1)
+        print(ret)
+
+        ret = self.log.get_user_resources("franky")
+        print(ret)
+
+        ret = self.log.get_user_resources("klaus")
+        print(ret)
 
     def test_termination(self):
 
