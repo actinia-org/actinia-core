df552a1a641f1bba58daf90817fc4c3e054163c6
AnikaBettge <37300249+AnikaBettge@users.noreply.github.com>
2020-10-20 07:38:00 +0200

    Update tests/test_strds_management.py
    Co-authored-by: Markus Neteler <neteler@gmail.com>

--- tests/test_strds_management.py
+++ tests/test_strds_management.py
@@ -42,7 +42,7 @@ strds_mapset = 'modis_lst'
 strds_url = (URL_PREFIX +
     '/locations/%(location)s/mapsets/%(mapset)s/strds'
     % {'location': location, 'mapset': strds_mapset})
-srtds_data = 'LST_Day_monthly'
+strds_data = 'LST_Day_monthly'
 
 
 class STRDSTestCase(ActiniaResourceTestCaseBase):
