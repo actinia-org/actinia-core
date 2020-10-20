49fe3eaa22b93c682eab5845e41bf7dffbf1adcc
AnikaBettge <37300249+AnikaBettge@users.noreply.github.com>
2020-10-20 07:37:12 +0200

    Update tests/test_strds_raster_management.py
    Co-authored-by: Markus Neteler <neteler@gmail.com>

--- tests/test_strds_raster_management.py
+++ tests/test_strds_raster_management.py
@@ -43,7 +43,7 @@ strds_mapset = 'modis_lst'
 strds_url = (URL_PREFIX +
     '/locations/%(location)s/mapsets/%(mapset)s/strds'
     % {'location': location, 'mapset': strds_mapset})
-srtds_data = 'LST_Day_monthly'
+strds_data = 'LST_Day_monthly'
 new_mapset = "raster_test_mapset"
 
 
