e8f40f9042431b9e5387a9e65b19bb92b331d251
anikaweinmann <aweinmann@mundialis.de>
2021-02-26 15:56:12 +0100

    add mapset_sizes

--- src/actinia_core/endpoints.py
+++ src/actinia_core/endpoints.py
@@ -66,7 +66,7 @@ from .resources.raster_renderer import SyncEphemeralRasterRendererResource
 from .resources.raster_renderer import SyncEphemeralRasterRGBRendererResource
 from .resources.raster_renderer import SyncEphemeralRasterShapeRendererResource
 from .resources.strds_renderer import SyncEphemeralSTRDSRendererResource
-from .resources.process_chain_monitoring import MaxMapsetSizeResource
+from .resources.process_chain_monitoring import MaxMapsetSizeResource, MapsetSizeResource, MapsetSizeRenderResource
 
 
 __license__ = "GPLv3"
@@ -164,8 +164,9 @@ def create_core_endpoints():
     flask_api.add_resource(SyncResourceStorageResource, '/resource_storage')
 
     # Endpoints for monitoring a process chain
-    flask_api.add_resource(MaxMapsetSizeResource, '/resources/<string:user_id>/<string:resource_id>/maxmapsetsize')
-    # flask_api.add_resource(MapsetSizeResource, '/resources/<string:user_id>/<string:resource_id>/mapsetsize')
+    flask_api.add_resource(MapsetSizeResource, '/resources/<string:user_id>/<string:resource_id>/mapsetsizes')
+    flask_api.add_resource(MaxMapsetSizeResource, '/resources/<string:user_id>/<string:resource_id>/mapsetsizes/max')
+    flask_api.add_resource(MapsetSizeRenderResource, '/resources/<string:user_id>/<string:resource_id>/mapsetsizes/render')
 
 
 
