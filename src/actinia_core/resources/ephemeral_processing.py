59c55d56ea7d1e540662f2c831a4cb88ead775d3
anikaweinmann <aweinmann@mundialis.de>
2021-05-27 10:58:22 +0200

    set maintainer to mundialis

--- src/actinia_core/resources/ephemeral_processing.py
+++ src/actinia_core/resources/ephemeral_processing.py
@@ -61,6 +61,7 @@ from .resource_base import ResourceBase
 __license__ = "GPLv3"
 __author__ = "Sören Gebbert, Anika Weinmann"
 __copyright__ = "Copyright 2016-2021, Sören Gebbert and mundialis GmbH & Co. KG"
+__maintainer__ = "mundialis"
 
 
 class AsyncEphemeralResource(ResourceBase):
