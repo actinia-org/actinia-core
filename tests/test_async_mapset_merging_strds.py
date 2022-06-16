88f49834746b8399b597177b16a7ff5899384ae5
anikaweinmann <aweinmann@mundialis.de>
2022-06-16 23:19:19 +0200

    linting

--- tests/test_async_mapset_merging_strds.py
+++ tests/test_async_mapset_merging_strds.py
@@ -418,7 +418,7 @@ class AsyncMapsetMergingSTRDS(ActiniaResourceTestCaseBase):
 
         self.waitAsyncStatusAssertHTTP(
             rv, headers=self.admin_auth_header, http_status=200, status="finished")
-        rv_test = self.server.get(URL_PREFIX + f'/locations/nc_spm_08/mapsets/{self.user_mapset}/strds'
+        self.server.get(URL_PREFIX + f'/locations/nc_spm_08/mapsets/{self.user_mapset}/strds')
 
         # check if strds 'modis' and 'modis2' is in mapset
         self.check_strds_in_mapset(['modis', 'modis2'])
