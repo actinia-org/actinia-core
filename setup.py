84810db1315cda8e15091cae854d059314a7c51f
Carmen <carmen.tawalika@gmail.com>
2022-08-11 16:01:54 +0200

    cleanup metadata files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 # performance processing of geographical data that uses GRASS GIS for
 # computational tasks. For details, see https://actinia.mundialis.de/
 #
-# Copyright (c) 2016-2018 Sören Gebbert and mundialis GmbH & Co. KG
+# Copyright (c) 2016-2022 Sören Gebbert and mundialis GmbH & Co. KG
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -33,29 +33,8 @@
 import sys
 from setuptools import setup
 
-# Add here console scripts and other entry points in ini-style format
-entry_points = """
-[console_scripts]
-# script_name = actinia_core.module:function
-# For example:
-# fibonacci = actinia_core.skeleton:run
-"""
-
-
 def setup_package():
-    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
-    sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=['pyscaffold>=3.0a0,<3.1a0', "rasterio==1.2.10"] + sphinx,
-          entry_points=entry_points,
-          use_pyscaffold=True,
-          scripts=['scripts/actinia-user',
-                   'scripts/actinia-bench',
-                   'scripts/actinia-algebra',
-                   'scripts/rq_custom_worker',
-                   'scripts/rq_starter',
-                   'scripts/webhook-server',
-                   'scripts/actinia-server'])
-
+    setup()
 
 if __name__ == "__main__":
     setup_package()
