cf2bbd0669afdbfa49e17edc1195169a51300667
Carmen <carmen.tawalika@gmail.com>
2024-06-14 16:04:10 +0200

    fix import error

--- src/actinia_core/__init__.py
+++ src/actinia_core/__init__.py
@@ -21,11 +21,10 @@
 #
 #######
 
-from pkg_resources import get_distribution, DistributionNotFound
-
+import importlib.metadata
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
+    __version__ = importlib.metadata.version(dist_name)
+except Exception:
     __version__ = "unknown"
