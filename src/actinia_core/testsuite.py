edf025bf6289f61b0f8163be3090c280cde0ea41
Carmen <carmen.tawalika@gmail.com>
2024-12-05 09:01:55 +0100

    fix tests

--- src/actinia_core/testsuite.py
+++ src/actinia_core/testsuite.py
@@ -35,6 +35,7 @@ from werkzeug.datastructures import Headers
 from actinia_api import URL_PREFIX
 
 from .version import init_versions
+from .health_check import health_check
 from actinia_core.core.common.app import flask_app
 from actinia_core.core.common import redis_interface
 from actinia_core.core.common.config import global_config
@@ -140,6 +141,7 @@ class ActiniaTestCaseBase(unittest.TestCase):
 
     # set project_url_part to "locations" if GRASS GIS version < 8.4
     init_versions()
+    health_check = health_check
     from .version import G_VERSION
 
     grass_version_s = G_VERSION["version"]
