8075d127f1c7cef69d8a11b9b81bf51f81a1ba39
Lina Krisztian <krisztian@mundialis.de>
2025-04-08 07:55:26 +0200

    replace redis by valkey function

--- src/actinia_core/core/redis_lock.py
+++ src/actinia_core/core/redis_lock.py
@@ -110,7 +110,7 @@ class RedisLockingInterface(object):
             kwargs["password"] = password
         self.connection_pool = valkey.ConnectionPool(**kwargs)
         del kwargs
-        self.redis_server = valkey.StrictRedis(
+        self.redis_server = valkey.StrictValkey(
             connection_pool=self.connection_pool
         )
 
